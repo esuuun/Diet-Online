b158c9004226f845a4b1d71d8a4cbbf485159c62
esuuun <ragendoft05@gmail.com>
2024-01-02 07:47:00 +0700

    nyoba

---
+++ main.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector(".hamburger")
+const menu = document.querySelector(".menu")
+function menu_btn(){
+    hamburger.classList.toggle("active")
+    menu.classList.toggle("active")
+}
